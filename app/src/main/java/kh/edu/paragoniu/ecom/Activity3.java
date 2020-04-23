765b916989236e7f0e4f644f682be1828cad6921
leapkh <leapkh@yahoo.com>
2020-04-23 11:21:29 +0700

    Design layout files for startup, login, and dashboard activities.

--- app/src/main/java/kh/edu/paragoniu/ecom/Activity3.java
+++
@@ -1,6 +0,0 @@
-package kh.edu.paragoniu.ecom;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class Activity3 extends AppCompatActivity {
-}
--- app/src/main/java/kh/edu/paragoniu/ecom/StartupActivity.java
+++ app/src/main/java/kh/edu/paragoniu/ecom/StartupActivity.java
@@ -1,16 +1,18 @@
 package kh.edu.paragoniu.ecom;
 
-import android.os.Bundle;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Activity2 extends AppCompatActivity {
+import android.os.Bundle;
+
+public class StartupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity2);
+        setContentView(R.layout.activity_startup);
     }
+
+
 }
